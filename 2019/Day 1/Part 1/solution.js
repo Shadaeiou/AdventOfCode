fa61a4fed991564f905cfe56c3b1fa45ffb7c28e
Burke Blazer <bblazer@agintegrated.com>
2019-12-02 10:40:19 -0500

    Day 1 and 2

---
+++ 2019/Day 1/Part 1/solution.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+(async () => {
+    var totalFuel = fs.readFileSync(__dirname + '/input.txt', { encoding: 'utf8' }).split("\n").reduce((currentFuel, mass) => {
+        return currentFuel + Math.floor(mass / 3 - 2);
+    }, 0);
+
+    console.log(`Answer: ${totalFuel}`);
+})();
---
+++ 2019/Day 1/Part 2/solution.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+
+const calcFuel = (mass) => {
+    return Math.floor(mass / 3 - 2);
+}
+
+(async () => {
+    var input        = fs.readFileSync(__dirname + '/input.txt', { encoding: 'utf8' });
+    var totalFuel    = input.split("\n").reduce((currentFuel, mass) => {
+        var fuelReq  = calcFuel(mass);
+        var totalReq = (fuelReq > 0) ? fuelReq : 0;
+        while (fuelReq > 0) {
+            fuelReq  = calcFuel(fuelReq);
+            totalReq += (fuelReq > 0) ? fuelReq : 0;
+        }
+        return currentFuel + totalReq;
+    }, 0);
+
+    console.log(`Answer: ${totalFuel}`);
+})();
